ef14cf38650795f5fc6ef6e09f60369b843920d5
Nicholas Cramer <73135495+NicholasCramer@users.noreply.github.com>
2023-12-06 15:49:50 -0700

    Update main.py

--- day-2-cube-conundrum/main.py
+++ day-2-cube-conundrum/main.py
@@ -35,3 +35,27 @@ with open(filename, 'r') as input_file:
     for game in possible_games:
         total += game
     print(total)
+
+# Part 2
+# What is the fewest number of cubes of each color that could have made each game possible?
+# What is the sum of the power of these sets?
+
+with open(filename, 'r') as input_file:
+    sum_of_powers = 0
+    for game_id, line in enumerate(input_file.readlines(), 1):
+        cube_sets = line.strip().split(": ")[1].split("; ")
+
+        bag = {
+            'red': 0,
+            'green': 0,
+            "blue": 0
+        }
+
+        for cube_set in cube_sets:
+            cubes = cube_set.split(", ")
+            for cube in cubes:
+                count, color = cube.split(" ")
+                bag[color] = max(bag[color], int(count))
+
+        sum_of_powers += bag['red'] * bag['green'] * bag["blue"]
+        print(sum_of_powers)
